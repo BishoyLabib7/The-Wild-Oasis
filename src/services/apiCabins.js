92fc2a6904e13dbba162483c5ff65ed72b1b961a
bishoy labib <bishoylabib78@gmail.com>
2025-01-25 19:55:25 +0200

    apiCabins.js

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,11 +1,22 @@
 import supabase from "./supabase";
 
-export async function getCabain() {
+export async function getCabin() {
   let { data, error } = await supabase.from("cabins").select("*");
 
   if (error) {
     console.error(error);
     throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be deleded");
   }
 
   return data;
