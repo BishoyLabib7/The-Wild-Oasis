2a965b2e08e2c7142b2ad51a6c0e88b6439fc662
bishoy labib <bishoylabib78@gmail.com>
2025-01-25 19:55:01 +0200

    Cabin.jsx

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,13 +1,8 @@
-import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabain } from "../services/apiCabins";
 import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
-  useEffect(function () {
-    getCabain().then((data) => console.log(data));
-  }, []);
   return (
     <>
       {" "}
