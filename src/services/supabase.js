a8820b36f0a959094812dfe1177008b776d9effd
bishoy labib <bishoylabib78@gmail.com>
2025-02-11 19:14:45 +0200

    supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://sshbeyhvlcntuowhmfyb.supabase.co";
+export const supabaseUrl = "https://sshbeyhvlcntuowhmfyb.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzaGJleWh2bGNudHVvd2htZnliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc2NTUxMzgsImV4cCI6MjA1MzIzMTEzOH0.1eHfp1ESaNcjJqyIFrpHGW4zlCFUaEQ9QtaTVThk6l8";
 const supabase = createClient(supabaseUrl, supabaseKey);
