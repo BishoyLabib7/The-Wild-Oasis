79d99c870730b68c6c2838a131bb3f1b18c25dea
bishoy labib <bishoylabib78@gmail.com>
2025-02-12 12:12:13 +0200

    CreateCabinForm

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -10,33 +10,33 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createEditCabin } from "../../services/apiCabins";
 import toast from "react-hot-toast";
 import FormRow from "../../ui/FormRow";
+/*
+const FormRow = styled.div`
+  display: grid;
+  align-items: center;
+  grid-template-columns: 24rem 1fr 1.2fr;
+  gap: 2.4rem;
 
-// const FormRow = styled.div`
-//   display: grid;
-//   align-items: center;
-//   grid-template-columns: 24rem 1fr 1.2fr;
-//   gap: 2.4rem;
+  padding: 1.2rem 0;
 
-//   padding: 1.2rem 0;
+  &:first-child {
+    padding-top: 0;
+  }
 
-//   &:first-child {
-//     padding-top: 0;
-//   }
+  &:last-child {
+    padding-bottom: 0;
+  }
 
-//   &:last-child {
-//     padding-bottom: 0;
-//   }
+  &:not(:last-child) {
+    border-bottom: 1px solid var(--color-grey-100);
+  }
 
-//   &:not(:last-child) {
-//     border-bottom: 1px solid var(--color-grey-100);
-//   }
-
-//   &:has(button) {
-//     display: flex;
-//     justify-content: flex-end;
-//     gap: 1.2rem;
-//   }
-// `;
+  &:has(button) {
+    display: flex;
+    justify-content: flex-end;
+    gap: 1.2rem;
+  }
+`;
 
 const Label = styled.label`
   font-weight: 500;
@@ -46,7 +46,7 @@ const Error = styled.span`
   font-size: 1.4rem;
   color: var(--color-red-700);
 `;
-
+*/
 function CreateCabinForm() {
   const { register, handleSubmit, reset, getValues, formState } = useForm();
 
@@ -65,7 +65,8 @@ function CreateCabinForm() {
   });
 
   function onSumbit(data) {
-    mutate(data);
+    console.log(data.image[0]);
+    // mutate({ ...data, image: data.image[0] });
   }
 
   function onError(errors) {
@@ -142,7 +143,13 @@ function CreateCabinForm() {
       </FormRow>
 
       <FormRow label="Cabin photo" error={errors?.image?.message}>
-        <FileInput id="image" accept="image/*" {...register("image")} />
+        <FileInput
+          id="image"
+          accept="image/*"
+          {...register("image", {
+            required: "This field is required",
+          })}
+        />
       </FormRow>
 
       <FormRow>
